1d1a04ae7fc7d22216e274bce2f1b9fa2387ec06
Joel Verhagen <joel.verhagen@gmail.com>
2017-11-11 16:43:26 -0800

    Add extension to make DI easier

--- ExplorePackages/Program.cs
+++ ExplorePackages/Program.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Knapcode.ExplorePackages.Commands;
 using Knapcode.ExplorePackages.Entities;
-using Knapcode.ExplorePackages.Logic;
 using Knapcode.ExplorePackages.Support;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using NuGet.Common;
-using NuGet.Configuration;
-using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 
 namespace Knapcode.ExplorePackages
@@ -160,67 +156,10 @@ namespace Knapcode.ExplorePackages
         private static ServiceCollection InitializeServiceCollection(ExplorePackagesSettings settings)
         {
             var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddExplorePackages(settings);
+
             serviceCollection.AddSingleton<ILogger, ConsoleLogger>();
-            serviceCollection.AddSingleton(
-                x => new HttpClientHandler
-                {
-                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
-                });
-            serviceCollection.AddSingleton(
-                x =>
-                {
-                    var httpClient = new HttpClient(x.GetRequiredService<HttpClientHandler>());
-                    httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", UserAgent.UserAgentString);
-                    return httpClient;
-                });
-            serviceCollection.AddSingleton(
-                x => new HttpSource(
-                    new PackageSource(settings.V3ServiceIndex),
-                    () =>
-                    {
-                        var httpClientHandler = x.GetRequiredService<HttpClientHandler>();
-                        var httpMessageHandler = new PassThroughHandler
-                        {
-                            InnerHandler = httpClientHandler
-                        };
-                        return Task.FromResult<HttpHandlerResource>(new HttpHandlerResourceV3(
-                            httpClientHandler,
-                            httpMessageHandler));
-                    },
-                    NullThrottle.Instance));
-            serviceCollection.AddTransient(
-                x => new PackagePathProvider(settings.PackagePath));
-
-            serviceCollection.AddTransient(x => settings.Clone());
-            serviceCollection.AddTransient<PackageQueryProcessor>();
-            serviceCollection.AddTransient<CatalogToDatabaseProcessor>();
-            serviceCollection.AddTransient<CatalogToNuspecsProcessor>();
-            serviceCollection.AddTransient<NuspecDownloader>();
-            serviceCollection.AddTransient<RemoteCursorReader>();
-            serviceCollection.AddTransient<PortDiscoverer>();
-            serviceCollection.AddTransient<SearchServiceUrlDiscoverer>();
-            serviceCollection.AddTransient<SearchServiceCursorReader>();
-            serviceCollection.AddTransient<PackageQueryContextBuilder>();
-
-            serviceCollection.AddSingleton<ServiceIndexCache>();
-            serviceCollection.AddTransient<GalleryClient>();
-            serviceCollection.AddTransient<V2Client>();
-            serviceCollection.AddTransient<PackagesContainerClient>();
-            serviceCollection.AddTransient<FlatContainerClient>();
-            serviceCollection.AddTransient<RegistrationClient>();
-            serviceCollection.AddTransient<SearchClient>();
-
-            serviceCollection.AddTransient<GalleryConsistencyService>();
-            serviceCollection.AddTransient<V2ConsistencyService>();
-            serviceCollection.AddTransient<FlatContainerConsistencyService>();
-            serviceCollection.AddTransient<PackagesContainerConsistencyService>();
-            serviceCollection.AddTransient<RegistrationOriginalConsistencyService>();
-            serviceCollection.AddTransient<RegistrationGzippedConsistencyService>();
-            serviceCollection.AddTransient<RegistrationSemVer2ConsistencyService>();
-            serviceCollection.AddTransient<SearchLoadBalancerConsistencyService>();
-            serviceCollection.AddTransient<SearchSpecificInstancesConsistencyService>();
-            serviceCollection.AddTransient<PackageConsistencyService>();
-            serviceCollection.AddTransient<CrossCheckConsistencyService>();
 
             serviceCollection.AddTransient<PackageQueriesCommand>();
             serviceCollection.AddTransient<FetchCursorsCommand>();
@@ -229,42 +168,6 @@ namespace Knapcode.ExplorePackages
             serviceCollection.AddTransient<ShowQueryResultsCommand>();
             serviceCollection.AddTransient<ShowRepositoriesCommand>();
             serviceCollection.AddTransient<CheckPackageCommand>();
-
-            serviceCollection.AddTransient<FindIdsEndingInDotNumberNuspecQuery>();
-            serviceCollection.AddTransient<FindEmptyDependencyVersionsNuspecQuery>();
-            serviceCollection.AddTransient<FindRepositoriesNuspecQuery>();
-            serviceCollection.AddTransient<FindInvalidDependencyVersionsNuspecQuery>();
-            serviceCollection.AddTransient<FindMissingDependencyVersionsNuspecQuery>();
-            serviceCollection.AddTransient<FindMissingDependencyIdsNuspecQuery>();
-            serviceCollection.AddTransient<FindPackageTypesNuspecQuery>();
-            serviceCollection.AddTransient<FindSemVer2PackageVersionsNuspecQuery>();
-            serviceCollection.AddTransient<FindSemVer2DependencyVersionsNuspecQuery>();
-            serviceCollection.AddTransient<FindFloatingDependencyVersionsNuspecQuery>();
-            
-            if (settings.RunConsistencyChecks)
-            {
-                serviceCollection.AddTransient<IPackageQuery, HasV2DiscrepancyPackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasPackagesContainerDiscrepancyPackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasFlatContainerDiscrepancyPackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasRegistrationDiscrepancyInOriginalHivePackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasRegistrationDiscrepancyInGzippedHivePackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasRegistrationDiscrepancyInSemVer2HivePackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasSearchDiscrepancyPackageQuery>();
-                serviceCollection.AddTransient<IPackageQuery, HasCrossCheckDiscrepancyPackageQuery>();
-            }
-
-            // Add all of the .nuspec queries as package queries.
-            var nuspecQueryDescriptors = serviceCollection
-                .Where(x => typeof(INuspecQuery).IsAssignableFrom(x.ServiceType))
-                .ToList();
-            foreach (var nuspecQueryDescriptor in nuspecQueryDescriptors)
-            {
-                serviceCollection.AddTransient<IPackageQuery>(x =>
-                {
-                    var nuspecQuery = (INuspecQuery) x.GetRequiredService(nuspecQueryDescriptor.ImplementationType);
-                    return new NuspecPackageQuery(nuspecQuery);
-                });
-            }
 
             return serviceCollection;
         }
