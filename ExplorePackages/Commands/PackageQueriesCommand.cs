c2ce3857ba51e1c32e1cb200a6ad91b5a05ed1ec
Joel Verhagen <jver@microsoft.com>
2017-11-17 08:59:48 -0800

    Auto-retry PackageQueriesCommand

--- ExplorePackages/Commands/PackageQueriesCommand.cs
+++ ExplorePackages/Commands/PackageQueriesCommand.cs
@@ -1,22 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Knapcode.ExplorePackages.Logic;
+using NuGet.Common;
 
 namespace Knapcode.ExplorePackages.Commands
 {
     public class PackageQueriesCommand : ICommand
     {
         private readonly PackageQueryProcessor _processor;
+        private readonly ILogger _log;
 
-        public PackageQueriesCommand(PackageQueryProcessor processor)
+        public PackageQueriesCommand(PackageQueryProcessor processor, ILogger log)
         {
             _processor = processor;
+            _log = log;
         }
 
         public async Task ExecuteAsync(IReadOnlyList<string> args, CancellationToken token)
         {
-            await _processor.ProcessAsync(token);
+            var complete = false;
+            do
+            {
+                try
+                {
+                    await _processor.ProcessAsync(token);
+                    complete = true;
+                }
+                catch (Exception e)
+                {
+                    _log.LogError("An exception was thrown while processing package queries: " + Environment.NewLine + e);
+                }
+            }
+            while (!complete);
         }
 
         public bool IsDatabaseRequired(IReadOnlyList<string> args)
