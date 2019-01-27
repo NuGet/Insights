1e05b392c52d45483aa647df047fffd040caa6f1
Joel Verhagen <joel.verhagen@gmail.com>
2019-01-26 22:52:06 -0800

    Fix cursor service

--- src/ExplorePackages.Logic/Services/CursorService.cs
+++ src/ExplorePackages.Logic/Services/CursorService.cs
@@ -9,8 +9,7 @@ namespace Knapcode.ExplorePackages.Logic
 {
     public class CursorService
     {
-        // private static readonly DateTimeOffset DefaultCursor = DateTimeOffset.MinValue;
-        private static readonly DateTimeOffset DefaultCursor = DateTimeOffset.UtcNow.AddHours(-1);
+        private static readonly DateTimeOffset DefaultCursor = DateTimeOffset.MinValue;
         private readonly EntityContextFactory _entityContextFactory;
 
         public CursorService(
