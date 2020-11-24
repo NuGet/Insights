c8daf91f750cdab32d8cac76aea8dcb79e47799d
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-23 21:44:05 -0800

    Add Pulumi infrastructure

---
+++ src/ExplorePackages.Infrastructure/MyStack.cs
@@ -0,0 +1,56 @@
+using Pulumi;
+using Pulumi.Azure.AppService;
+using Pulumi.Azure.AppService.Inputs;
+using Pulumi.Azure.AppInsights;
+using Pulumi.Azure.Core;
+using Pulumi.Azure.Storage;
+
+namespace Knapcode.ExplorePackages
+{
+    public class MyStack : Stack
+    {
+        public MyStack()
+        {
+            var resourceGroup = new ResourceGroup(Deployment.Instance.ProjectName + "-" + Deployment.Instance.StackName);
+
+            var storageAccount = new Account("explorepackages", new AccountArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                AccountReplicationType = "LRS",
+                AccountTier = "Standard",
+            });
+
+            var appInsights = new Insights("explorepackages", new InsightsArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                ApplicationType = "web",
+            });
+
+            var workerPlan = new Plan("ExplorePackagesWorker", new PlanArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                Kind = "FunctionApp",
+                Sku = new PlanSkuArgs
+                {
+                    Tier = "Dynamic",
+                    Size = "Y1",
+                },
+            });
+
+            var workerApp = new FunctionApp("ExplorePackagesWorker", new FunctionAppArgs
+            {
+                ResourceGroupName = resourceGroup.Name,
+                AppServicePlanId = workerPlan.Id,
+                StorageAccountName = storageAccount.Name,
+                StorageAccountAccessKey = storageAccount.PrimaryAccessKey,
+                AppSettings = new InputMap<string>
+                {
+                    { "APPINSIGHTS_INSTRUMENTATIONKEY", appInsights.InstrumentationKey },
+                    { "APPLICATIONINSIGHTS_CONNECTION_STRING", appInsights.ConnectionString },
+                    { "Knapcode.ExplorePackages:StorageConnectionString", storageAccount.PrimaryConnectionString },
+                },
+            });
+        }
+    }
+}
+
---
+++ src/ExplorePackages.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Pulumi;
+
+namespace Knapcode.ExplorePackages
+{
+    public class Program
+    {
+        static Task<int> Main() => Deployment.RunAsync<MyStack>();
+    }
+}
