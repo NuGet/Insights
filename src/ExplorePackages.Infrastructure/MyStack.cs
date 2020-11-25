e3c5f78000b6113c9e6ec20f8a0d102c75481ba3
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-24 17:28:28 -0800

    Add new stacks for another iteration

--- src/ExplorePackages.Infrastructure/MyStack.cs
+++ src/ExplorePackages.Infrastructure/MyStack.cs
@@ -4,6 +4,7 @@ using Pulumi.Azure.AppService.Inputs;
 using Pulumi.Azure.AppInsights;
 using Pulumi.Azure.Core;
 using Pulumi.Azure.Storage;
+using System.Text.RegularExpressions;
 
 namespace Knapcode.ExplorePackages
 {
@@ -11,22 +12,25 @@ namespace Knapcode.ExplorePackages
     {
         public MyStack()
         {
-            var resourceGroup = new ResourceGroup(Deployment.Instance.ProjectName + "-" + Deployment.Instance.StackName);
+            var projectStack = Deployment.Instance.ProjectName + "-" + Deployment.Instance.StackName;
+            var stackAlpha = Regex.Replace(Deployment.Instance.StackName, "[^a-z0-9]", string.Empty, RegexOptions.IgnoreCase);
 
-            var storageAccount = new Account("explorepackages", new AccountArgs
+            var resourceGroup = new ResourceGroup(projectStack);
+
+            var storageAccount = new Account("explore" + stackAlpha.ToLowerInvariant(), new AccountArgs
             {
                 ResourceGroupName = resourceGroup.Name,
                 AccountReplicationType = "LRS",
                 AccountTier = "Standard",
             });
 
-            var appInsights = new Insights("explorepackages", new InsightsArgs
+            var appInsights = new Insights("ExplorePackages" + stackAlpha, new InsightsArgs
             {
                 ResourceGroupName = resourceGroup.Name,
                 ApplicationType = "web",
             });
 
-            var workerPlan = new Plan("ExplorePackagesWorker", new PlanArgs
+            var workerPlan = new Plan("ExplorePackagesWorker" + stackAlpha, new PlanArgs
             {
                 ResourceGroupName = resourceGroup.Name,
                 Kind = "FunctionApp",
@@ -39,7 +43,7 @@ namespace Knapcode.ExplorePackages
 
             var config = new Config();
 
-            var workerApp = new FunctionApp("ExplorePackagesWorker", new FunctionAppArgs
+            var workerApp = new FunctionApp("ExplorePackagesWorker" + stackAlpha, new FunctionAppArgs
             {
                 ResourceGroupName = resourceGroup.Name,
                 AppServicePlanId = workerPlan.Id,
