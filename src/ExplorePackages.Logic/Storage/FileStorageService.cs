0cb0a9177d43d27aa63fa013dbb736177b09ffbd
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-27 09:32:13 -0800

    Update and move around dependencies

--- src/ExplorePackages.Logic/Storage/FileStorageService.cs
+++ src/ExplorePackages.Logic/Storage/FileStorageService.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 
 namespace Knapcode.ExplorePackages.Logic
