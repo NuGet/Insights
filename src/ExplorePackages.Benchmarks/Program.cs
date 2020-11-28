dcd2fb4ba0e61fa087f55cb5c47db91ba066bfe8
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-27 18:45:13 -0800

    Add real benchmark ...

---
+++ src/ExplorePackages.Benchmarks/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Knapcode.ExplorePackages.Worker;
+using Knapcode.ExplorePackages.Worker.FindPackageAssets;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Knapcode.ExplorePackages
+{
+    public class TheAppendResultStorageServiceClass
+    {
+        public class TheCompactAsyncMethod : IDisposable
+        {
+            private readonly ServiceProvider _serviceProvider;
+            private readonly AppendResultStorageService _appendResultStorageService;
+
+            public TheCompactAsyncMethod()
+            {
+                var serviceCollection = new ServiceCollection();
+
+                serviceCollection.AddExplorePackages("Knapcode.ExplorePackages.Benchmarks");
+                serviceCollection.AddExplorePackagesWorker();
+
+                serviceCollection.Configure<ExplorePackagesWorkerSettings>(x =>
+                {
+                    x.AppendResultStorageMode = AppendResultStorageMode.AppendBlob;
+                });
+
+                _serviceProvider = serviceCollection.BuildServiceProvider();
+                _appendResultStorageService = _serviceProvider.GetRequiredService<AppendResultStorageService>();
+            }
+
+            public void Dispose() => _serviceProvider.Dispose();
+
+            [Benchmark]
+            public async Task ExecuteAsync()
+            {
+                await _appendResultStorageService.CompactAsync<PackageAsset>(
+                   "findpackageassets",
+                   "findpackageassets",
+                   0,
+                   force: true,
+                   mergeExisting: true,
+                   PruneAssets);
+            }
+
+            private static IEnumerable<PackageAsset> PruneAssets(IEnumerable<PackageAsset> allAssets)
+            {
+                return allAssets
+                    .GroupBy(x => new { Id = x.Id.ToLowerInvariant(), Version = x.Version.ToLowerInvariant() }) // Group by unique package version
+                    .Select(g => g
+                        .GroupBy(x => x.ScanId) // Group package version assets by scan
+                        .OrderByDescending(x => x.First().Created) // Ignore all but the most recent scan of the most recent version of the package
+                        .OrderByDescending(x => x.First().ScanTimestamp)
+                        .First())
+                    .SelectMany(g => g)
+                    .OrderBy(x => x.Id, StringComparer.OrdinalIgnoreCase)
+                    .ThenBy(x => x.Version, StringComparer.OrdinalIgnoreCase)
+                    .Distinct()
+                    .ToList();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            BenchmarkRunner.Run<TheAppendResultStorageServiceClass.TheCompactAsyncMethod>(new DebugInProcessConfig());
+#else
+            BenchmarkRunner.Run<TheAppendResultStorageServiceClass.TheCompactAsyncMethod>();
+#endif
+        }
+    }
+}
