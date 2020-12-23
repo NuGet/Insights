462d569e9e52a2c8d676bffa9300ef11ab331753
Joel Verhagen <joel.verhagen@gmail.com>
2020-12-23 13:34:46 -0800

    Parallelize integration tests

--- test/ExplorePackages.Worker.Test/FindPackageAssembliesIntegrationTest.cs
+++ test/ExplorePackages.Worker.Test/FindPackageAssembliesIntegrationTest.cs
@@ -21,124 +21,175 @@ namespace Knapcode.ExplorePackages.Worker
 
         protected override string DestinationContainerName => Options.Value.FindPackageAssembliesContainerName;
 
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public async Task FindPackageAssemblies(bool bufferTempStreamsToMemory)
+        public class FindPackageAssemblies : FindPackageAssembliesIntegrationTest
         {
-            ConfigureSettings = x =>
+            public FindPackageAssemblies(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
             {
-                if (!bufferTempStreamsToMemory)
-                {
-                    x.MaxInMemoryTempStreamSize = 0;
-                }
-            };
+            }
 
-            Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+            [Fact]
+            public async Task Execute()
+            {
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
 
-            // Arrange
-            var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
-            var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
-            var max2 = DateTimeOffset.Parse("2020-11-27T19:36:50.4909042Z");
-            var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
+                // Arrange
+                var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
+                var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
+                var max2 = DateTimeOffset.Parse("2020-11-27T19:36:50.4909042Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
 
-            await CatalogScanService.InitializeAsync();
+                await CatalogScanService.InitializeAsync();
 
-            var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
-            cursor.Value = min0;
-            await CursorStorageService.UpdateAsync(cursor);
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
 
-            // Act
-            await UpdateFindPackageAssembliesAsync(max1);
+                // Act
+                await UpdateFindPackageAssembliesAsync(max1);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssembliesDir, Step1, 0);
-            await AssertOutputAsync(FindPackageAssembliesDir, Step1, 1);
-            await AssertOutputAsync(FindPackageAssembliesDir, Step1, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 2);
 
-            // Act
-            await UpdateFindPackageAssembliesAsync(max2);
+                // Act
+                await UpdateFindPackageAssembliesAsync(max2);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssembliesDir, Step2, 0);
-            await AssertOutputAsync(FindPackageAssembliesDir, Step1, 1); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssembliesDir, Step2, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssembliesDir, Step2, 0);
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 1); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssembliesDir, Step2, 2);
 
-            await VerifyExpectedContainersAsync();
+                await VerifyExpectedContainersAsync();
+            }
         }
 
-        [Fact]
-        public async Task FindPackageAssemblies_WithDelete()
+        public class FindPackageAssemblies_WithoutMemoryBuffering : FindPackageAssembliesIntegrationTest
         {
-            Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
-
-            // Arrange
-            HttpMessageHandlerFactory.OnSendAsync = async req =>
+            public FindPackageAssemblies_WithoutMemoryBuffering(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
             {
-                if (req.RequestUri.AbsolutePath.EndsWith("/behaviorsample.1.0.0.nupkg"))
-                {
-                    var newReq = Clone(req);
-                    newReq.RequestUri = new Uri($"http://localhost/{TestData}/behaviorsample.1.0.0.nupkg");
-                    return await TestDataHttpClient.SendAsync(newReq);
-                }
+            }
 
-                return null;
-            };
-            var min0 = DateTimeOffset.Parse("2020-12-20T02:37:31.5269913Z");
-            var max1 = DateTimeOffset.Parse("2020-12-20T03:01:57.2082154Z");
-            var max2 = DateTimeOffset.Parse("2020-12-20T03:03:53.7885893Z");
-            var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
+            [Fact]
+            public async Task Execute()
+            {
+                ConfigureSettings = x => x.MaxInMemoryTempStreamSize = 0;
 
-            await CatalogScanService.InitializeAsync();
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
 
-            var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
-            cursor.Value = min0;
-            await CursorStorageService.UpdateAsync(cursor);
+                // Arrange
+                var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
+                var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
 
-            // Act
-            await UpdateFindPackageAssembliesAsync(max1);
+                await CatalogScanService.InitializeAsync();
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 0);
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 1);
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 2);
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
 
-            // Act
-            await UpdateFindPackageAssembliesAsync(max2);
+                // Act
+                await UpdateFindPackageAssembliesAsync(max1);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 0); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 1); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step2, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssembliesDir, Step1, 2);
 
-            await VerifyExpectedContainersAsync();
+                await VerifyExpectedContainersAsync();
+            }
         }
 
-        [Fact]
-        public async Task FindPackageAssemblies_WithUnmanaged()
+        public class FindPackageAssemblies_WithDelete : FindPackageAssembliesIntegrationTest
         {
-            ConfigureWorkerSettings = x => x.AppendResultStorageBucketCount = 1;
+            public FindPackageAssemblies_WithDelete(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
+            {
+            }
 
-            Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+            [Fact]
+            public async Task Execute()
+            {
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
 
-            // Arrange
-            var min0 = DateTimeOffset.Parse("2018-08-29T04:22:56.6184931Z");
-            var max1 = DateTimeOffset.Parse("2018-08-29T04:24:40.3247223Z");
-            var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
+                // Arrange
+                HttpMessageHandlerFactory.OnSendAsync = async req =>
+                {
+                    if (req.RequestUri.AbsolutePath.EndsWith("/behaviorsample.1.0.0.nupkg"))
+                    {
+                        var newReq = Clone(req);
+                        newReq.RequestUri = new Uri($"http://localhost/{TestData}/behaviorsample.1.0.0.nupkg");
+                        return await TestDataHttpClient.SendAsync(newReq);
+                    }
 
-            await CatalogScanService.InitializeAsync();
+                    return null;
+                };
+                var min0 = DateTimeOffset.Parse("2020-12-20T02:37:31.5269913Z");
+                var max1 = DateTimeOffset.Parse("2020-12-20T03:01:57.2082154Z");
+                var max2 = DateTimeOffset.Parse("2020-12-20T03:03:53.7885893Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
 
-            var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
-            cursor.Value = min0;
-            await CursorStorageService.UpdateAsync(cursor);
+                await CatalogScanService.InitializeAsync();
 
-            // Act
-            await UpdateFindPackageAssembliesAsync(max1);
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssemblies_WithUnmanagedDir, Step1, 0);
+                // Act
+                await UpdateFindPackageAssembliesAsync(max1);
 
-            await VerifyExpectedContainersAsync();
+                // Assert
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 2);
+
+                // Act
+                await UpdateFindPackageAssembliesAsync(max2);
+
+                // Assert
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 0); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step1, 1); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssemblies_WithDeleteDir, Step2, 2);
+
+                await VerifyExpectedContainersAsync();
+            }
+        }
+
+        public class FindPackageAssemblies_WithUnmanaged : FindPackageAssembliesIntegrationTest
+        {
+            public FindPackageAssemblies_WithUnmanaged(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
+            {
+            }
+
+            [Fact]
+            public async Task Execute()
+            {
+                ConfigureWorkerSettings = x => x.AppendResultStorageBucketCount = 1;
+
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+
+                // Arrange
+                var min0 = DateTimeOffset.Parse("2018-08-29T04:22:56.6184931Z");
+                var max1 = DateTimeOffset.Parse("2018-08-29T04:24:40.3247223Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssemblies}";
+
+                await CatalogScanService.InitializeAsync();
+
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
+
+                // Act
+                await UpdateFindPackageAssembliesAsync(max1);
+
+                // Assert
+                await AssertOutputAsync(FindPackageAssemblies_WithUnmanagedDir, Step1, 0);
+
+                await VerifyExpectedContainersAsync();
+            }
         }
 
         private async Task UpdateFindPackageAssembliesAsync(DateTimeOffset max)
--- test/ExplorePackages.Worker.Test/FindPackageAssetsIntegrationTest.cs
+++ test/ExplorePackages.Worker.Test/FindPackageAssetsIntegrationTest.cs
@@ -20,91 +20,140 @@ namespace Knapcode.ExplorePackages.Worker
 
         protected override string DestinationContainerName => Options.Value.FindPackageAssetsContainerName;
 
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public async Task FindPackageAssets(bool allowBatching)
+        public class FindPackageAssets : FindPackageAssetsIntegrationTest
         {
-            ConfigureWorkerSettings = x => x.AllowBatching = allowBatching;
+            public FindPackageAssets(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
+            {
+            }
 
-            Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+            [Fact]
+            public async Task Execute()
+            {
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
 
-            // Arrange
-            var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
-            var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
-            var max2 = DateTimeOffset.Parse("2020-11-27T19:36:50.4909042Z");
-            var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssets}";
+                // Arrange
+                var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
+                var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
+                var max2 = DateTimeOffset.Parse("2020-11-27T19:36:50.4909042Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssets}";
 
-            await CatalogScanService.InitializeAsync();
+                await CatalogScanService.InitializeAsync();
 
-            var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
-            cursor.Value = min0;
-            await CursorStorageService.UpdateAsync(cursor);
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
 
-            // Act
-            await UpdateFindPackageAssetsAsync(max1);
+                // Act
+                await UpdateFindPackageAssetsAsync(max1);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssetsDir, Step1, 0);
-            await AssertOutputAsync(FindPackageAssetsDir, Step1, 1);
-            await AssertOutputAsync(FindPackageAssetsDir, Step1, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 2);
 
-            // Act
-            await UpdateFindPackageAssetsAsync(max2);
+                // Act
+                await UpdateFindPackageAssetsAsync(max2);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssetsDir, Step2, 0);
-            await AssertOutputAsync(FindPackageAssetsDir, Step1, 1); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssetsDir, Step2, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssetsDir, Step2, 0);
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 1); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssetsDir, Step2, 2);
 
-            await VerifyExpectedContainersAsync();
+                await VerifyExpectedContainersAsync();
+            }
         }
 
-        [Fact]
-        public async Task FindPackageAssets_WithDelete()
+        public class FindPackageAssets_WithoutBatching : FindPackageAssetsIntegrationTest
         {
-            Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
-
-            // Arrange
-            HttpMessageHandlerFactory.OnSendAsync = async req =>
+            public FindPackageAssets_WithoutBatching(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
             {
-                if (req.RequestUri.AbsolutePath.EndsWith("/behaviorsample.1.0.0.nupkg"))
+            }
+
+            [Fact]
+            public async Task Execute()
+            {
+                ConfigureWorkerSettings = x => x.AllowBatching = false;
+
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+
+                // Arrange
+                var min0 = DateTimeOffset.Parse("2020-11-27T19:34:24.4257168Z");
+                var max1 = DateTimeOffset.Parse("2020-11-27T19:35:06.0046046Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssets}";
+
+                await CatalogScanService.InitializeAsync();
+
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
+
+                // Act
+                await UpdateFindPackageAssetsAsync(max1);
+
+                // Assert
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssetsDir, Step1, 2);
+
+                await VerifyExpectedContainersAsync();
+            }
+        }
+
+        public class FindPackageAssets_WithDelete : FindPackageAssetsIntegrationTest
+        {
+            public FindPackageAssets_WithDelete(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory)
+                : base(output, factory)
+            {
+            }
+
+            [Fact]
+            public async Task Execute()
+            {
+                Logger.LogInformation("Settings: " + Environment.NewLine + JsonConvert.SerializeObject(Options.Value, Formatting.Indented));
+
+                // Arrange
+                HttpMessageHandlerFactory.OnSendAsync = async req =>
                 {
-                    var newReq = Clone(req);
-                    newReq.RequestUri = new Uri($"http://localhost/{TestData}/behaviorsample.1.0.0.nupkg");
-                    return await TestDataHttpClient.SendAsync(newReq);
-                }
+                    if (req.RequestUri.AbsolutePath.EndsWith("/behaviorsample.1.0.0.nupkg"))
+                    {
+                        var newReq = Clone(req);
+                        newReq.RequestUri = new Uri($"http://localhost/{TestData}/behaviorsample.1.0.0.nupkg");
+                        return await TestDataHttpClient.SendAsync(newReq);
+                    }
 
-                return null;
-            };
-            var min0 = DateTimeOffset.Parse("2020-12-20T02:37:31.5269913Z");
-            var max1 = DateTimeOffset.Parse("2020-12-20T03:01:57.2082154Z");
-            var max2 = DateTimeOffset.Parse("2020-12-20T03:03:53.7885893Z");
-            var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssets}";
+                    return null;
+                };
+                var min0 = DateTimeOffset.Parse("2020-12-20T02:37:31.5269913Z");
+                var max1 = DateTimeOffset.Parse("2020-12-20T03:01:57.2082154Z");
+                var max2 = DateTimeOffset.Parse("2020-12-20T03:03:53.7885893Z");
+                var cursorName = $"CatalogScan-{CatalogScanType.FindPackageAssets}";
 
-            await CatalogScanService.InitializeAsync();
+                await CatalogScanService.InitializeAsync();
 
-            var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
-            cursor.Value = min0;
-            await CursorStorageService.UpdateAsync(cursor);
+                var cursor = await CursorStorageService.GetOrCreateAsync(cursorName);
+                cursor.Value = min0;
+                await CursorStorageService.UpdateAsync(cursor);
 
-            // Act
-            await UpdateFindPackageAssetsAsync(max1);
+                // Act
+                await UpdateFindPackageAssetsAsync(max1);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 0);
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 1);
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 0);
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 1);
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 2);
 
-            // Act
-            await UpdateFindPackageAssetsAsync(max2);
+                // Act
+                await UpdateFindPackageAssetsAsync(max2);
 
-            // Assert
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 0); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 1); // This file is unchanged.
-            await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step2, 2);
+                // Assert
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 0); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step1, 1); // This file is unchanged.
+                await AssertOutputAsync(FindPackageAssets_WithDeleteDir, Step2, 2);
 
-            await VerifyExpectedContainersAsync();
+                await VerifyExpectedContainersAsync();
+            }
         }
 
         private async Task UpdateFindPackageAssetsAsync(DateTimeOffset max)
