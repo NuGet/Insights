91d4d1dcb91295707794ed32303f124d36492bdc
Joel Verhagen <joel.verhagen@gmail.com>
2021-02-21 21:10:44 -0800

    Re-arrange driver enum

--- src/ExplorePackages.Worker.Logic/CatalogScan/CatalogScanDriverType.cs
+++ src/ExplorePackages.Worker.Logic/CatalogScan/CatalogScanDriverType.cs
@@ -3,6 +3,14 @@
     public enum CatalogScanDriverType
     {
         /// <summary>
+        /// Implemented by <see cref="FindLatestCatalogLeafScan.LatestCatalogLeafScanStorage"/> and <see cref="FindLatestLeafDriver{T}"/>.
+        /// This is a helper catalog scan used by <see cref="CatalogIndexScanMessageProcessor"/> when the driver returns
+        /// <see cref="CatalogIndexScanResult.ExpandLatestLeaves"/>. This allows another driver to only process the latest
+        /// catalog leaf per version instead of duplicating effort per version which is inevitable in the NuGet.org catalog.
+        /// </summary>
+        Internal_FindLatestCatalogLeafScan,
+
+        /// <summary>
         /// Implemented by <see cref="LoadPackageArchive.LoadPackageArchiveDriver"/>. Downloads interesting parts of the .nupkg
         /// file stored in the V3 flat container and stores the data in Azure Table Storage for other drivers to use.
         /// </summary>
@@ -45,14 +53,6 @@
         /// This driver records the latest catalog leaf URL for each package version to Azure Table Storage.
         /// </summary>
         FindLatestPackageLeaf,
-
-        /// <summary>
-        /// Implemented by <see cref="FindLatestCatalogLeafScan.LatestCatalogLeafScanStorage"/> and <see cref="FindLatestLeafDriver{T}"/>.
-        /// This is a helper catalog scan used by <see cref="CatalogIndexScanMessageProcessor"/> when the driver returns
-        /// <see cref="CatalogIndexScanResult.ExpandLatestLeaves"/>. This allows another driver to only process the latest
-        /// catalog leaf per version instead of duplicating effort per version which is inevitable in the NuGet.org catalog.
-        /// </summary>
-        Internal_FindLatestCatalogLeafScan,
 
         /// <summary>
         /// Implemented by <see cref="PackageManifestToCsv.PackageManifestToCsvDriver"/>. This driver reads all package
