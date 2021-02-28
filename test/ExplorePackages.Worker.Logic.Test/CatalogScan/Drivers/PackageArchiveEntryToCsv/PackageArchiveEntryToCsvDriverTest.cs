5da54b39e0d97ce649aa07d2c92ca30c772777eb
Loic Sharma <sharma.loic@gmail.com>
2021-02-28 11:55:05 -0800

    Clean

--- test/ExplorePackages.Worker.Logic.Test/CatalogScan/Drivers/PackageArchiveEntryToCsv/PackageArchiveEntryToCsvDriverTest.cs
+++ test/ExplorePackages.Worker.Logic.Test/CatalogScan/Drivers/PackageArchiveEntryToCsv/PackageArchiveEntryToCsvDriverTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Knapcode.ExplorePackages.Worker.PackageArchiveEntryToCsv;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
