28762403101995e518463213e5789aad8cb085b0
Joel Verhagen <joel.verhagen@gmail.com>
2021-03-22 21:08:47 -0700

    Add tests for CursorStorageService

---
+++ test/ExplorePackages.Worker.Logic.Test/Cursors/CursorStorageServiceTest.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Knapcode.ExplorePackages.Worker;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Knapcode.ExplorePackages.Worker
+{
+    public class CursorStorageServiceTest : IClassFixture<CursorStorageServiceTest.Fixture>
+    {
+        public class TheGetOrCreateAsyncMethod : CursorStorageServiceTest
+        {
+            public TheGetOrCreateAsyncMethod(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+            {
+            }
+
+            [Fact]
+            public async Task ReturnsExistingCursor()
+            {
+                var value = new DateTimeOffset(2020, 1, 1, 12, 30, 0, TimeSpan.Zero);
+                await Table.InsertEntitiesAsync(new[] { new CursorTableEntity(CursorName) { Value = value } });
+
+                var cursor = await Target.GetOrCreateAsync(CursorName);
+
+                Assert.Equal(value, cursor.Value);
+            }
+
+            [Fact]
+            public async Task CreatesNewCursor()
+            {
+                var cursor = await Target.GetOrCreateAsync(CursorName);
+
+                Assert.Equal(CursorTableEntity.Min, cursor.Value);
+                var entities = await GetEntitiesAsync<CursorTableEntity>();
+                var entity = Assert.Single(entities);
+                Assert.Equal(cursor.Value, entity.Value);
+                Assert.Equal(cursor.PartitionKey, entity.PartitionKey);
+                Assert.Equal(cursor.RowKey, entity.RowKey);
+                Assert.Equal(CursorName, entity.Name);
+                Assert.Equal(cursor.ETag, entity.ETag);
+                Assert.Equal(cursor.Timestamp, entity.Timestamp);
+            }
+
+            [Fact]
+            public async Task HasExpectedProperties()
+            {
+                var cursor = await Target.GetOrCreateAsync(CursorName);
+
+                var entities = await GetEntitiesAsync<DynamicTableEntity>();
+                var entity = Assert.Single(entities);
+                Assert.Equal(new[] { "Value" }, entity.Properties.Keys.OrderBy(x => x).ToArray());
+            }
+        }
+
+        public class TheUpdateAsyncMethod : CursorStorageServiceTest
+        {
+            public TheUpdateAsyncMethod(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+            {
+            }
+
+            [Fact]
+            public async Task UpdatesValue()
+            {
+                var value = new DateTimeOffset(2020, 1, 1, 12, 30, 0, TimeSpan.Zero);
+                var cursor = await Target.GetOrCreateAsync(CursorName);
+                cursor.Value = value;
+
+                await Target.UpdateAsync(cursor);
+
+                cursor = await Target.GetOrCreateAsync(CursorName);
+                Assert.Equal(value, cursor.Value);
+            }
+
+            [Fact]
+            public async Task FailsIfAnotherActorUpdatedEntity()
+            {
+                var cursor = await Target.GetOrCreateAsync(CursorName);
+                await Target.UpdateAsync(await Target.GetOrCreateAsync(CursorName));
+                var value = new DateTimeOffset(2020, 1, 1, 12, 30, 0, TimeSpan.Zero);
+
+                await Assert.ThrowsAsync<StorageException>(() => Target.UpdateAsync(cursor));
+
+                cursor = await Target.GetOrCreateAsync(CursorName);
+                Assert.Equal(CursorTableEntity.Min, cursor.Value);
+            }
+        }
+
+        public CursorStorageServiceTest(Fixture fixture, ITestOutputHelper output)
+        {
+            _fixture = fixture;
+            _output = output;
+            CursorNamePrefix = StorageUtility.GenerateDescendingId().ToString();
+            CursorName = CursorNamePrefix + "-a";
+        }
+
+        private readonly Fixture _fixture;
+        private readonly ITestOutputHelper _output;
+
+        public string CursorNamePrefix { get; }
+        public string CursorName { get; }
+        public CursorStorageService Target => new CursorStorageService(
+            _fixture.ServiceClientFactory,
+            _fixture.Options.Object,
+            _output.GetLogger<CursorStorageService>());
+        public CloudTable Table => _fixture.GetTable();
+
+        protected async Task<IReadOnlyList<T>> GetEntitiesAsync<T>() where T : ITableEntity, new()
+        {
+            return await Table.GetEntitiesAsync<T>(
+                partitionKey: string.Empty,
+                minRowKey: CursorNamePrefix,
+                maxRowKey: CursorNamePrefix + char.MaxValue,
+                _output.GetTelemetryClient().StartQueryLoopMetrics());
+        }
+
+        public class Fixture : IAsyncLifetime
+        {
+            public Fixture()
+            {
+                Options = new Mock<IOptions<ExplorePackagesWorkerSettings>>();
+                Settings = new ExplorePackagesWorkerSettings
+                {
+                    StorageConnectionString = TestSettings.StorageConnectionString,
+                    CursorTableName = TestSettings.NewStoragePrefix() + "1c1",
+                };
+                Options.Setup(x => x.Value).Returns(() => Settings);
+                ServiceClientFactory = new ServiceClientFactory(Options.Object);
+            }
+
+            public Mock<IOptions<ExplorePackagesWorkerSettings>> Options { get; }
+            public ExplorePackagesWorkerSettings Settings { get; }
+            public ServiceClientFactory ServiceClientFactory { get; }
+
+            public async Task InitializeAsync()
+            {
+                await GetTable().CreateIfNotExistsAsync();
+            }
+
+            public async Task DisposeAsync()
+            {
+                await GetTable().DeleteIfExistsAsync();
+            }
+
+            public CloudTable GetTable()
+            {
+                return ServiceClientFactory
+                    .GetStorageAccount()
+                    .CreateCloudTableClient()
+                    .GetTableReference(Options.Object.Value.CursorTableName);
+            }
+        }
+    }
+}
