bdc6d72d11d6654d812a37d8a5f99aa846a83f8c
Joel Verhagen <joel.verhagen@gmail.com>
2021-03-27 19:25:21 -0700

    Move to newer Azure.Storage.Queues version to resolve bug

---
+++ test/ExplorePackages.Worker.Logic.Test/Queue/MessageEnqueuerIntegrationTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Azure.Storage.Queues.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Knapcode.ExplorePackages.Worker
+{
+    public class MessageEnqueuerIntegrationTest : BaseWorkerLogicIntegrationTest
+    {
+        public MessageEnqueuerIntegrationTest(ITestOutputHelper output, DefaultWebApplicationFactory<StaticFilesStartup> factory) : base(output, factory)
+        {
+        }
+
+        public IMessageEnqueuer Target => Host.Services.GetRequiredService<IMessageEnqueuer>();
+
+        [Fact]
+        public async Task CanEnqueueAscii()
+        {
+            await Target.InitializeAsync();
+            var messages = new[] { new CatalogLeafScanMessage { LeafId = "Newtonsoft" } };
+
+            await Target.EnqueueAsync(messages);
+
+            PeekedMessage message = await (await ServiceClientFactory.GetQueueServiceClientAsync())
+                .GetQueueClient(Options.Value.WorkerQueueName)
+                .PeekMessageAsync();
+            Assert.Equal(@"{""n"":""cls"",""v"":1,""d"":{""r"":""Newtonsoft""}}", message.Body.ToString());
+        }
+
+        [Fact]
+        public async Task CanEnqueueNonAscii()
+        {
+            await Target.InitializeAsync();
+            var messages = new[] { new CatalogLeafScanMessage { LeafId = "Nëwtönsöft" } };
+
+            await Target.EnqueueAsync(messages);
+
+            PeekedMessage message = await (await ServiceClientFactory.GetQueueServiceClientAsync())
+                .GetQueueClient(Options.Value.WorkerQueueName)
+                .PeekMessageAsync();
+            Assert.Equal(@"{""n"":""cls"",""v"":1,""d"":{""r"":""Nëwtönsöft""}}", message.Body.ToString());
+        }
+    }
+}
