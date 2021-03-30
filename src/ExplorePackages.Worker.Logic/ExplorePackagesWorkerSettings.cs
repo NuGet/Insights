45a4567bc5e94eaac3644368ea3ebb084fd38bc1
Joel Verhagen <jver@microsoft.com>
2021-03-30 16:22:56 -0700

    Off by one error

--- src/ExplorePackages.Worker.Logic/ExplorePackagesWorkerSettings.cs
+++ src/ExplorePackages.Worker.Logic/ExplorePackagesWorkerSettings.cs
@@ -17,7 +17,7 @@ namespace Knapcode.ExplorePackages.Worker
             OnlyKeepLatestInStreamWriterUpdater = true;
             MoveTempToHome = false;
             DisabledDrivers = new List<CatalogScanDriverType>();
-            OldCatalogIndexScansToKeep = 10;
+            OldCatalogIndexScansToKeep = 9;
 
             AutoStartCatalogScanUpdate = false;
             AutoStartDownloadToCsv = false;
