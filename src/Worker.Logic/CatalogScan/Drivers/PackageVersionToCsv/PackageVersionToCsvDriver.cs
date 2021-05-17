e0d84405535d1a6feed867d357cb1667e4d29177
Joel Verhagen <joel.verhagen@gmail.com>
2021-05-16 20:08:16 -0700

    Fix bug in SemVerOrder

--- src/Worker.Logic/CatalogScan/Drivers/PackageVersionToCsv/PackageVersionToCsvDriver.cs
+++ src/Worker.Logic/CatalogScan/Drivers/PackageVersionToCsv/PackageVersionToCsvDriver.cs
@@ -48,20 +48,34 @@ namespace NuGet.Insights.Worker.PackageVersionToCsv
             // Fetch all of the known versions for this package ID.
             var entities = await _storageService.GetAsync(item.PackageId);
 
+            // Parse the NuGet version for all entities
+            var entityToNuGetVersion = entities
+                .ToDictionary(
+                    x => x,
+                    x => NuGetVersion.Parse(x.PackageVersion),
+                    ReferenceEqualityComparer<PackageVersionEntity>.Instance);
+            var entityToSemVerOrder = entityToNuGetVersion
+                .OrderBy(x => x.Value)
+                .Select((x, i) => (Entity: x.Key, SemVerOrder: i))
+                .ToDictionary(
+                    x => x.Entity,
+                    x => x.SemVerOrder,
+                    entityToNuGetVersion.Comparer);
+
             // Find the set of versions that can possibly the latest.
-            var versions = entities
+            var listedVersions = entities
                 .Where(x => x.LeafType != CatalogLeafType.PackageDelete) // Deleted versions can't be the latest
                 .Where(x => x.IsListed.Value) // Only listed versions can be latest
-                .Select(x => (Entity: x, Version: NuGetVersion.Parse(x.PackageVersion), IsSemVer2: x.SemVerType.Value.IsSemVer2()))
+                .Select(x => (Entity: x, Version: entityToNuGetVersion[x], IsSemVer2: x.SemVerType.Value.IsSemVer2()))
                 .OrderByDescending(x => x.Version)
                 .ToList();
-            var semVer1Versions = versions.Where(x => !x.IsSemVer2).ToList();
+            var semVer1Versions = listedVersions.Where(x => !x.IsSemVer2).ToList();
 
             // Determine the four definitions of "latest". Reminds me of NuGet.org Azure Search implementation...
             var latest = semVer1Versions.FirstOrDefault();
             var latestStable = semVer1Versions.Where(x => !x.Version.IsPrerelease).FirstOrDefault();
-            var latestSemVer2 = versions.FirstOrDefault();
-            var latestStableSemVer2 = versions.Where(x => !x.Version.IsPrerelease).FirstOrDefault();
+            var latestSemVer2 = listedVersions.FirstOrDefault();
+            var latestStableSemVer2 = listedVersions.Where(x => !x.Version.IsPrerelease).FirstOrDefault();
 
             // Map all entities to CSV records.
             var records = new List<PackageVersionRecord>();
@@ -70,7 +84,7 @@ namespace NuGet.Insights.Worker.PackageVersionToCsv
                 var entity = entities[i];
                 records.Add(new PackageVersionRecord(scanId, scanTimestamp, entity)
                 {
-                    SemVerOrder = entities.Count - (i + 1),
+                    SemVerOrder = entityToSemVerOrder[entity],
                     IsLatest = ReferenceEquals(entity, latest.Entity),
                     IsLatestStable = ReferenceEquals(entity, latestStable.Entity),
                     IsLatestSemVer2 = ReferenceEquals(entity, latestSemVer2.Entity),
