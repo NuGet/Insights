82891f041e5f713ddf401d2d4be28deabe76dc90
Joel Verhagen <jver@microsoft.com>
2021-05-24 17:31:45 -0700

    Add missing scope to allow reading group membership

--- src/Website/Startup.cs
+++ src/Website/Startup.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -73,7 +73,7 @@ namespace NuGet.Insights.Website
             if (initialSettings.AllowedGroups.Any())
             {
                 microsoftIdentityBuilder
-                    .EnableTokenAcquisitionToCallDownstreamApi()
+                    .EnableTokenAcquisitionToCallDownstreamApi(new[] { "user.read" })
                     .AddInMemoryTokenCaches()
                     .AddMicrosoftGraph();
             }
