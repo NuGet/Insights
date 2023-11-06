f974ce138a0b965fd301a8a910ace6c35e50831e
Joel Verhagen <joel.verhagen@gmail.com>
2023-11-06 11:14:53 -0500

    Test flakiness: skip real Kusto ingestions on macOS
    This is flaky. Maybe 1 out of every 10 times there is a hang.
    It appears to be when the Kusto assemblies are loaded during DI.
    Since we don't deploy to macOS we won't investigate it for now.

--- test/Worker.Logic.Test/TestSupport/KustoFactAttribute.cs
+++ test/Worker.Logic.Test/TestSupport/KustoFactAttribute.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace NuGet.Insights.Worker
@@ -18,6 +19,11 @@ namespace NuGet.Insights.Worker
             {
                 Skip = "This Fact is skipped because the Kusto environment variables are not set. " +
                     $"Set both {TestSettings.KustoConnectionStringEnv} and {TestSettings.KustoDatabaseNameEnv}.";
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Skip = "This Fact is skipped because Kusto initialization randomly hangs in macOS CI environments.";
             }
         }
     }
