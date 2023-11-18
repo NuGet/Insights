417e46dd5a1b72ebe62480143fc0ad75c53b7070
Joel Verhagen <joel.verhagen@gmail.com>
2023-11-18 14:57:12 -0500

    Refactor GetTitle and GetRuntime view helpers

--- src/Website/ExtensionMethods.cs
+++ src/Website/ExtensionMethods.cs
@@ -1,18 +1,21 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using NuGet.Insights.Worker;
 
 namespace NuGet.Insights.Website
 {
-    /// <summary>
-    /// Source: https://stackoverflow.com/a/20411015
-    /// </summary>
     public static class ExtensionMethods
     {
+        /// <summary>
+        /// Source: https://stackoverflow.com/a/20411015
+        /// </summary>
         public static string IsSelected(this IHtmlHelper htmlHelper, string controllers, string actions, string cssClass = "active")
         {
             var currentAction = htmlHelper.ViewContext.RouteData.Values["action"] as string;
@@ -23,6 +26,34 @@ namespace NuGet.Insights.Website
 
             return acceptedActions.Contains(currentAction) && acceptedControllers.Contains(currentController) ?
                 cssClass : string.Empty;
+        }
+
+        public static string GetRuntime(this DateTimeOffset? completed, DateTimeOffset? started)
+        {
+            if (!started.HasValue)
+            {
+                return string.Empty;
+            }
+
+            var runtime = completed.GetValueOrDefault(DateTimeOffset.UtcNow) - started.Value;
+            var runtimeStr = runtime.ToString("d\\.hh\\:mm\\:ss", CultureInfo.InvariantCulture);
+            if (runtimeStr.StartsWith("0.", StringComparison.Ordinal))
+            {
+                runtimeStr = runtimeStr.Substring(2);
+            }
+
+            return runtimeStr;
+        }
+
+        public static string GetTitle(this CatalogScanDriverType type)
+        {
+            var title = type.ToString().Humanize();
+
+            title = title.Replace(" csv", " CSV", StringComparison.Ordinal);
+
+            title = title.Replace("Nu get package explorer", "NuGet Package Explorer", StringComparison.Ordinal);
+
+            return title;
         }
     }
 }
