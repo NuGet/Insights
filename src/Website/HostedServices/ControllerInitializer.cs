32d9197f681ddd0d3cb55fa25688789d55546b5e
Joel Verhagen <joel.verhagen@gmail.com>
2024-08-07 10:04:06 -0400

    Allow ControllerInitializer to retry

--- src/Website/HostedServices/ControllerInitializer.cs
+++ src/Website/HostedServices/ControllerInitializer.cs
@@ -6,30 +6,73 @@ using NuGet.Insights.Worker.Workflow;
 
 namespace NuGet.Insights.Website
 {
-    public class ControllerInitializer
+    public class ControllerInitializer : IAsyncDisposable
     {
-        private readonly Lazy<Task> _initalize;
+        private Task _currentTask;
+        private Task _loopTask;
+        private readonly CancellationTokenSource _cts;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly ILogger<ControllerInitializer> _logger;
 
         public ControllerInitializer(
-            CatalogScanService catalogScanService,
-            TimerExecutionService timerExecutionService,
-            WorkflowService workflowService,
-            ViewModelFactory viewModelFactory)
+            IServiceProvider serviceProvider,
+            ILogger<ControllerInitializer> logger)
         {
-            _initalize = new Lazy<Task>(async () =>
-            {
-                await catalogScanService.InitializeAsync();
-                await timerExecutionService.InitializeAsync();
-                await workflowService.InitializeAsync();
+            _cts = new CancellationTokenSource();
+            _serviceProvider = serviceProvider;
+            _logger = logger;
+            _currentTask = InitializeInternalAsync();
+            _loopTask = InitializeLoopAsync();
+        }
 
-                // warm up
-                await viewModelFactory.GetAdminViewModelAsync();
-            });
+        private async Task InitializeLoopAsync()
+        {
+            do
+            {
+                try
+                {
+                    await _currentTask;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "The controller initializer failed.");
+                    await Task.Delay(TimeSpan.FromSeconds(5));
+                    _currentTask = InitializeInternalAsync();
+                }
+            }
+            while (!_cts.IsCancellationRequested && !_currentTask.IsCompletedSuccessfully);
+        }
+
+        private async Task InitializeInternalAsync()
+        {
+            if (_cts.IsCancellationRequested)
+            {
+                return;
+            }
+
+            await Task.Yield();
+
+            using var scope = _serviceProvider.CreateScope();
+
+            // initialize
+            await scope.ServiceProvider.GetRequiredService<CatalogScanService>().InitializeAsync();
+            await scope.ServiceProvider.GetRequiredService<TimerExecutionService>().InitializeAsync();
+            await scope.ServiceProvider.GetRequiredService<WorkflowService>().InitializeAsync();
+
+            // warm up
+            await scope.ServiceProvider.GetRequiredService<ViewModelFactory>().GetAdminViewModelAsync();
         }
 
         public async Task InitializeAsync()
         {
-            await _initalize.Value;
+            await _currentTask;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _cts.CancelAsync();
+            await _loopTask;
+            _cts.Dispose();
         }
     }
 }
