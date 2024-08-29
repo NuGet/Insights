ae657aab51acb533c15fc17694ea4f30b71407cc
Joel Verhagen <joel.verhagen@gmail.com>
2024-08-29 12:20:27 -0400

    Add time delta display for timer control

--- src/Website/wwwroot/js/site.js
+++ src/Website/wwwroot/js/site.js
@@ -3,6 +3,17 @@
     // Inspired by: https://scottdorman.blog/2018/08/18/saving-bootstrap-component-state/
     const collapseStateKey = "collapse-state";
 
+    // Source: https://stackoverflow.com/a/75988895
+    const debounce = (callback, wait) => {
+        let timeoutId = null;
+        return (...args) => {
+            window.clearTimeout(timeoutId);
+            timeoutId = window.setTimeout(() => {
+                callback(...args);
+            }, wait);
+        };
+    }
+
     function getFragmentKey() {
         if (window.location.hash.length > 0) {
             return window.location.hash.substr(1);
@@ -52,67 +63,81 @@
         return id.substr(0, id.length - "-content".length);
     }
 
-    $(function () {
-        function setCheckboxes() {
-            var card = $(this).parents('.card')
-            var onlyLatestLeaves = card.find('input[name="onlyLatestLeaves"]');
-            var useCustomCursor = card.find('input[name="useCustomCursor"]');
-            var useBucketRanges = card.find('input[name="useBucketRanges"]');
+    function setCheckboxes() {
+        var card = $(this).parents('.card')
+        var onlyLatestLeaves = card.find('input[name="onlyLatestLeaves"]');
+        var useCustomCursor = card.find('input[name="useCustomCursor"]');
+        var useBucketRanges = card.find('input[name="useBucketRanges"]');
 
-            var customCursorChecked = false;
-            var bucketRangesChecked = false;
+        var customCursorChecked = false;
+        var bucketRangesChecked = false;
 
-            if (this == useCustomCursor[0]) {
-                customCursorChecked = useCustomCursor.prop('checked');
-            } else if (this == useBucketRanges[0]) {
-                customCursorChecked = false;
-                bucketRangesChecked = useBucketRanges.prop('checked');
-            }
-            
-            useCustomCursor.prop('checked', customCursorChecked);
-            card.find('.custom-cursor').toggle(customCursorChecked);
-            card.find('button[name="overrideCursor"]').toggle(customCursorChecked);
-            
-            useBucketRanges.prop('checked', bucketRangesChecked);
-            card.find('.bucket-ranges').toggle(bucketRangesChecked);
-            
-            onlyLatestLeaves.prop('disabled', bucketRangesChecked);
+        if (this == useCustomCursor[0]) {
+            customCursorChecked = useCustomCursor.prop('checked');
+        } else if (this == useBucketRanges[0]) {
+            customCursorChecked = false;
+            bucketRangesChecked = useBucketRanges.prop('checked');
         }
 
-        $('input[name="useCustomCursor"]').on('change', setCheckboxes);
-        $('input[name="useBucketRanges"]').on('change', setCheckboxes);
+        useCustomCursor.prop('checked', customCursorChecked);
+        card.find('.custom-cursor').toggle(customCursorChecked);
+        card.find('button[name="overrideCursor"]').toggle(customCursorChecked);
 
-        $('button').on('click', function () {
-            if ($(this).hasClass('btn-danger')) {
-                var message = $(this).data('message');
-                if (!message) {
-                    message = "Are you sure you want to do this?";
-                }
+        useBucketRanges.prop('checked', bucketRangesChecked);
+        card.find('.bucket-ranges').toggle(bucketRangesChecked);
 
-                return confirm(message);
-            }
-        })
+        onlyLatestLeaves.prop('disabled', bucketRangesChecked);
+    }
 
-        $('.collapse-remember').on('hide.bs.collapse show.bs.collapse', function (e) {
-            $(this).data('kc.hidden', e.type == 'hide');
-            setCollapseState(this.id, e.type == 'hide');
-        });
-
-        $('.collapse-remember').on('hide.bs.collapse', function (e) {
-            if (getKeyFromContentId(this.id) == getFragmentKey()) {
-                history.replaceState("", document.title, window.location.pathname + window.location.search);
-            }
-        });
-
-        var fragmentKey = getFragmentKey();
-        if (fragmentKey) {
-            $('#' + fragmentKey + '-content').collapse('show');
+    var updateNextRun = function (el) {
+        var thisEl = $(el)
+        var parsed = dateFns.parseISO(thisEl.val());
+        var nextRunEl = thisEl.parents('.card').find('.next-run-delta')
+        if (isNaN(parsed)) {
+            thisEl.addClass("is-invalid")
+            nextRunEl.text("(invalid ISO timestamp)")
+        } else {
+            thisEl.removeClass("is-invalid")
+            nextRunEl.text("(" + dateFns.formatDistance(parsed, new Date(), { addSuffix: true }) + ")")
         }
+    }
 
-        $('.card [data-toggle="collapse"]').dblclick(function () {
-            var hidden = $(this).parents(".card").find('.collapse-remember').data('kc.hidden');
-            $('.collapse-remember').collapse(hidden ? 'hide' : 'show');
-        });
+    $('input[name="useCustomCursor"]').on('change', setCheckboxes);
+    $('input[name="useBucketRanges"]').on('change', setCheckboxes);
+
+    $('input[name="nextRun"]').on('change keyup paste', debounce(function (e) { updateNextRun(e.target) }, 100))
+    $('input[name="nextRun"]').each(function (_, el) { updateNextRun(el) })
+
+    $('button').on('click', function () {
+        if ($(this).hasClass('btn-danger')) {
+            var message = $(this).data('message');
+            if (!message) {
+                message = "Are you sure you want to do this?";
+            }
+
+            return confirm(message);
+        }
+    })
+
+    $('.collapse-remember').on('hide.bs.collapse show.bs.collapse', function (e) {
+        $(this).data('kc.hidden', e.type == 'hide');
+        setCollapseState(this.id, e.type == 'hide');
+    });
+
+    $('.collapse-remember').on('hide.bs.collapse', function (e) {
+        if (getKeyFromContentId(this.id) == getFragmentKey()) {
+            history.replaceState("", document.title, window.location.pathname + window.location.search);
+        }
+    });
+
+    var fragmentKey = getFragmentKey();
+    if (fragmentKey) {
+        $('#' + fragmentKey + '-content').collapse('show');
+    }
+
+    $('.card [data-toggle="collapse"]').dblclick(function () {
+        var hidden = $(this).parents(".card").find('.collapse-remember').data('kc.hidden');
+        $('.collapse-remember').collapse(hidden ? 'hide' : 'show');
     });
 
     restoreCollapseState();
