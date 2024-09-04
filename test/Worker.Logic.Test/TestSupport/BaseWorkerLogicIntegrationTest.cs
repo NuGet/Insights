ccd4da140bff99e24bccf5d105374bfb37c8a635
Joel Verhagen <joel.verhagen@gmail.com>
2024-09-04 08:48:44 -0400

    Fix typo

--- test/Worker.Logic.Test/TestSupport/BaseWorkerLogicIntegrationTest.cs
+++ test/Worker.Logic.Test/TestSupport/BaseWorkerLogicIntegrationTest.cs
@@ -556,7 +556,7 @@ namespace NuGet.Insights.Worker
                 "X-Cache-Info",
                 "X-CDN-Rewrite",
                 "X-Content-Type-Options",
-                "X-EC-BRR-Enable",
+                "X-EC-BBR-Enable",
                 "x-fd-int-roxy-purgeid",
                 "x-ms-lease-state",
                 "x-ms-request-id",
