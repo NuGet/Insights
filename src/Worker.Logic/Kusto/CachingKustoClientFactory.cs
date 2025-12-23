af82fafbde86ab46f60e3b3d91d6abc37a4c0aa3
Erick Yondon <8766776+erdembayar@users.noreply.github.com>
2025-12-22 18:02:50 -0800

    Fix error IDE0005: Using directive is unnecessary.

--- src/Worker.Logic/Kusto/CachingKustoClientFactory.cs
+++ src/Worker.Logic/Kusto/CachingKustoClientFactory.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Security.Cryptography.X509Certificates;
-using Azure.Core;
-using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
 using Kusto.Cloud.Platform.Utils;
 using Kusto.Data;
